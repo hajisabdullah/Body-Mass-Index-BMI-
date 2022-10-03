8a2f4815fbc778a9b8d364e9d709b3b6a58d5017
hajisabdullah <79291794+hajisabdullah@users.noreply.github.com>
2022-10-03 22:58:42 +0700

    Add files via upload

---
+++ lib/constants/constant.dart
@@ -0,0 +1,22 @@
+// import 'package:flutter/cupertino.dart' show Color, TextStyle;
+import 'package:flutter/material.dart'
+    show Color, Colors, FontWeight, TextStyle;
+
+const Color primaryColor = Color(0xff0a0e21);
+TextStyle? labelTextStyle = const TextStyle(fontSize: 18, color: Colors.white);
+//Color(0xff8d8e98),
+
+TextStyle numberTextStyle = const TextStyle(
+  fontSize: 50,
+  fontWeight: FontWeight.bold,
+  color: Colors.white,
+);
+
+const String underweightModerate = "Underweight (Moderate thinness)";
+const String underweightMild = "Underweight (Mild thinness)";
+const String underweightSevere = "Underweight (Severe thinness)";
+const String normal = "Normal";
+const String overweight = "Overweight";
+const String obeseI = "Obese (Class I)";
+const String obeseII = "Obese (Class II)";
+const String obeseIII = "Obese (Class III)";
